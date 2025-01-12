31f3cc44ff0c5c150a6496503756df10559ad5d0
IvanHHdz <hernivan336@outlook.com>
2025-01-11 19:04:23 -0600

    Add error handling and command documentation for directory operations

--- Code/commands.py
+++ Code/commands.py
@@ -1,43 +1,79 @@
 import os
-from format import color
+from format import color, p_error, p_elemets, p_color
 import re
+from functools import partial
+
+# * Get Elements
+def ge(rute):
+    e = [[],[]]
+    for item in os.listdir(rute):
+        c_rute = os.path.join(rute, item)
+        if os.path.isdir(c_rute):
+            e[1].append(True)
+        else:
+            e[1].append(False)
+        e[0].append(item)
+    return e
+
+# * Verication of Arguments - Rute
+def ver_arg_rut(rute):
+    if rute:
+        if re.match(r"^\./.+", rute):
+            return re.sub(r"^\.", f"{gr()}", rute)
+        elif re.match(r"^[\w/ \.-]*/.+", rute):
+            return rute
+        else:
+            p_error('Invalid Rute')
+    return gr()
+    
 
 # * List Elements
 def le(arg1 = None):
-    if arg1:
-        if re.match(r"^\./.+", arg1):
-            rute = re.sub(r"^\.", f"{gr()}", arg1)
-        else:
-            print(color("Red", "Invalid Rute!"))
-            return
-    else:
-        rute = gr()
-    for item in os.listdir(rute):
-        c_rute = os.path.join(rute, item)
-        if os.path.isfile(c_rute):
-            print(color('Blue', item))
-        elif os.path.isdir(c_rute):
-            print(color('Yellow', item))
-
-# * Show Tree
-def st():
-    pass
+    rute = ver_arg_rut(arg1)
+    p_elemets(ge(rute), "Cyan", "Magenta")
 
 # * Get Rute 
 def gr():
     return os.getcwd()
 
+# * Show Tree
+def st(arg1=None, t=1):
+    rute = ver_arg_rut(arg1)
+    ele = ge(rute)
+    if t == 1:
+        p_color(rute, "Cyan")
+    for i in range(len(ele[0])):
+        if ele[1][i]:
+            p_color(['|\t'*(t-1)+'|-> ',ele[0][i]],['Yellow','Cyan'])
+            st(rute+"/"+ele[0][i], t+1)
+
 # * Show Rute
 def sr():
     print(gr())
 
+# * Documentation
+def dc(arg1=None,st = False):
+    if st:
+        p_error("Try 'st' to see the documentation.")
+    else:
+        if arg1:
+            pass
+        else:
+            print("Valid commands:")
+            p = partial(p_color, c=["Green","White"])
+            p(["\tsr" , " - Show actual rute."])
+            p(["\tle <rute>"," - List the elements of a rute, the actual rute by default."])
+            p(["\tdc"," - Show the documentation."])
+            p(["\tst <rute>", " - Show a tree of directories from a rute, the actual rute by default."])
+    #TODO agregar la documentacion
+
 # * Commands
 def command(command):
     commands = {
-        "gr" : gr,
         "sr" : sr,
         "le" : le,
-        "tr" : st
+        "dc" : dc,
+        "st" : st
     }
     try:
         if len(command) == 1:
@@ -45,11 +81,12 @@ def command(command):
         else:
             return commands[command[0]](arg1 = command[1])
     except:
-        print(color("Rojo", "Sintax Error!"))
+        p_error("Sintax Error")
+        dc(st = True)
 
 def main():
-    usr = "le Code".split(' ')
-    command(usr)
+    dc()
+    st()
 
 if __name__ == "__main__":
     main()
--- Code/format.py
+++ Code/format.py
@@ -17,6 +17,21 @@ def color(color, text):
         color = [str(c) for c in color]
         return f"\033[38;2;{';'.join(color)}m{text}\033[0m"
 
+def p_error(msg):
+    print(color("Red", msg))
+    
+def p_color(msg, c):
+    if type(msg) == str:
+        print(color(c, msg))
+    else:
+        for i in range(len(msg)):
+            print(color(c[i], msg[i]),end="")
+        print()
+
+def p_elemets(l, c1, c2):
+    for i in range(len(l[0])):
+        p_color(l[0][i], c1 if l[1][i] else c2)
+
 def main():
     pass
 
